5f812f8a5a412f69060b8ad81b2f5af7846eff1a
Isaac Witmer <isaac.witmer@vistashare.com>
2015-01-19 16:57:47 -0500

    Include up-coming points, and make it work on the season page

--- Calculate_Town_Score.js
+++ Calculate_Town_Score.js
@@ -1,10 +1,25 @@
 loc = window.location.href;
-if(loc.indexOf('www.die2nite.com') != -1 && loc.indexOf('ghost/ingame') != -1){
-    score = 0;
-    $('td.days').each(function() {
+function getmsg(qrystr) {
+    score = 0, count = $(qrystr).length;
+    $(qrystr).each(function() {
         score += parseInt($(this).text());
     });
-    alert(score + ' (not including souls)');
-} else {
-    alert('Use this on a towns history page');
+    additional = (40 - count) * (parseInt($(qrystr + ':first').text()));
+    str = score + ' (not including souls)';
+    if (additional>0){
+        str += '\nand at least ' + additional + ' points if the rest (' + (40 - count) + ') die today\nTotalling: ' + (score + additional);
+    }
+    return str;
 }
+if(loc.indexOf('www.die2nite.com') != -1){
+    if (loc.indexOf('ghost/ingame') != -1){
+        str = getmsg('td.days');
+    } else if (loc.indexOf('ghost/ranking') != -1){
+        str = getmsg("table.details td:not('.uname'):not('.comment')");
+    } else {
+        str = 'This only works on Town history pages';
+    }
+} else {
+    str = 'This only works on die2nite.com';
+}
+alert(str);

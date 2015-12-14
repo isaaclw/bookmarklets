6ad53a72e85f53f7f07f003adb876fee7f477319
Isaac Witmer <isaac.witmer@vistashare.com>
2015-12-14 15:45:54 -0500

    Adds checks to the Twinoid message so that they run on the right pages

--- d2n/Create_Twinoid_Message_From_Town.js
+++ d2n/Create_Twinoid_Message_From_Town.js
@@ -1,5 +1,13 @@
-i = [];
-$('a.tid_user').each(function () {
-    i = i.concat($(this).attr('tid_id'));
-});
-_tid.askDiscuss(i.join(','));
+loc = window.location.href;
+if(loc.indexOf('www.die2nite.com') != -1 && loc.indexOf('city/co') != -1){
+    i = [];
+    $('a.tid_user').each(function () {
+        i = i.concat($(this).attr('tid_id'));
+    });
+    if (i.length === 0) {
+        alert("Can't find any citizens in this town, use it on the Citizen's page");
+    }
+    _tid.askDiscuss(i.join(','));
+} else {
+    alert("Use this on the list of citizens in town");
+}

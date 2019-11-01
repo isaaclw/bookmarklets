fec34abcdc51216fd3b1496911f14d5f6296629e
Isaac Witmer <isaac.witmer@vistashare.com>
2019-11-01 11:27:41 -0400

    Bookmarklet for vs trac

---
+++ general/trac-ify.js
@@ -0,0 +1,13 @@
+$j('table.vs-classic-grid tbody tr th').each(function(index, elem){
+    var header = $j(elem).text();
+    if(header.indexOf('DevSupport Ticket #') >= 0){
+        $j('table.vs-classic-grid tbody tr').each(function(i, tr){
+            var correct_td = $j($j(tr).children()[index]);
+            var ticketn = correct_td.text();
+            if(ticketn.length > 0) {
+                correct_td.html('<a href=\'https://trac.vistashare.net/ticket/' + ticketn + '\'>' + ticketn + '</a>');
+            }
+        });
+        return;
+    }
+});

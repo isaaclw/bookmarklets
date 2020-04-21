5a563964c6d8581a30535fc46fd699b9a286e666
Isaac Witmer <isaaclw@gmail.com>
2020-04-21 17:57:57 -0400

    Needs to be wrapped in a function to work

--- general/trac-ify.js
+++ general/trac-ify.js
@@ -1,13 +1,15 @@
-$j('table.vs-classic-grid tbody tr th').each(function(index, elem){
-    var header = $j(elem).text();
-    if(header.indexOf('DevSupport Ticket #') >= 0){
-        $j('table.vs-classic-grid tbody tr').each(function(i, tr){
-            var correct_td = $j($j(tr).children()[index]);
-            var ticketn = correct_td.text();
-            if(ticketn.length > 0) {
-                correct_td.html('<a href=\'https://trac.vistashare.net/ticket/' + ticketn + '\'>' + ticketn + '</a>');
-            }
-        });
-        return;
-    }
-});
+(function () {
+    $j('table.vs-classic-grid tbody tr th').each(function(index, elem){
+        var header = $j(elem).text();
+        if(header.indexOf('DevSupport Ticket #') >= 0){
+            $j('table.vs-classic-grid tbody tr').each(function(i, tr){
+                var correct_td = $j($j(tr).children()[index]);
+                var ticketn = correct_td.text();
+                if(ticketn.length > 0) {
+                    correct_td.html('<a href=\'https://trac.vistashare.net/ticket/' + ticketn + '\'>' + ticketn + '</a>');
+                }
+            });
+            return;
+        }
+    });
+}());
